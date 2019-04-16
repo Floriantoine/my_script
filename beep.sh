b665078b058f5a4bb28be8d4d4a2fa1295d93a0e
florian <florian.antoine@epitech.eu>
2019-04-16 15:57:48 +0200

    [PUSH]

--- beep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VAR=88
-VARM=600
-
-while [ "$VAR" -lt "$VARM" ]; do
-    beep -f $VAR
-    let "VAR++"
-done
--- cancer.sh
+++
@@ -1,8 +0,0 @@
-
-VAR=$(find /run/media/aflorian/3962-3034/music -iname "*$1*" | pmenu)
-
-echo $VAR
-
-while [ 1 ]; do
-ffplay "$VAR"
-done
